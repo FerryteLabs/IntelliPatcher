38c07df58144077e527aec74d8d2503d904866d7
StNiosem <bauch.aristide@gmail.com>
2023-11-23 20:13:51 +0100

    Updateed intelliMain patch command Linke INterface.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
 print("\nIntelliPatcher")
-print("by StNiosem\n\n")
+print("by StNiosem\n")
 
 lib_count = 0
 
-print("Importing libraries.")
-from password_maker import generate; print("Importing password_maker for UUIDs"); lib_count += 1
+print("Importing libraries.\n")
 
+# Libraries
+import time; print("Imported time for timers"); lib_count +=1; time.sleep(0.05)
+from password_maker import generate; print("Imported password_maker for UUIDs"); lib_count += 1; time.sleep(0.2)
+import fileinput; print("Imported fileinput for inputting patches"); lib_count += 1; time.sleep(0.5)
+import os; print("Imported os for system calls"); lib_count += 1
+
+# Check lib_count for values. Employ plural form if needed. Esle it displays error that value
+# isn't in the explected range (0 and up) and exit
 if (lib_count == 0) :
-    print("Imported no libraries.")
-
+    print("\nImported no libraries.")
 elif (lib_count == 1) :
     print("\nImported " + str(lib_count) + " Library")
-
 elif (lib_count > 1) :
     print("\nImported " + str(lib_count) + " Libraries")
-
 else :
-    print("unexpected value in counter lib_count")
+    print("\nUnexpected value in counter lib_count. Exiting")
+    exit()
